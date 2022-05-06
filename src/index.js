97d9bcf8f02a632a6319af3cb0cb48604ea318c3
王宇 <coder_wangyu@163.com>
2022-05-06 17:02:06 +0800

    init：初始化视角控件、抓手控件

--- src/index.js
+++ src/index.js
@@ -18,8 +18,26 @@ import Ygeometry from "./core/Geometry";
 import { Tooltip } from "antd";
 import iconAdd from "./assets/img/controllers/add.svg";
 import iconReduce from "./assets/img/controllers/reduce.svg";
+import iconHand from "./assets/img/controllers/hand.svg";
+import iconHandChoose from "./assets/img/controllers/hand-choose.svg";
+import iconTop from "./assets/img/controllers/top.svg";
+import iconTopChoose from "./assets/img/controllers/top-choose.svg";
+import iconRight from "./assets/img/controllers/right.svg";
+import iconRightChoose from "./assets/img/controllers/right-choose.svg";
+import iconBottom from "./assets/img/controllers/bottom.svg";
+import iconBottomChoose from "./assets/img/controllers/bottom-choose.svg";
+import iconLeft from "./assets/img/controllers/left.svg";
+import iconLeftChoose from "./assets/img/controllers/left-choose.svg";
 
 class MB extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      handChoose: false,
+      viewChoose: ""
+    }
+  }
+
   draw(w, h) {
     Ycamera.init(w, h)
     Yrenderer.init(w, h)
@@ -28,6 +46,7 @@ class MB extends Component {
     Yfloor.init()
     Ygeometry.initMainLeft(["MLbox", "MLcylinder", "MLcone", "MLsphere", "MLtorus", "MLicosahedron", "MLcapsule"])
     Yevents.initThreeClickEvent(document.getElementById('MB'))
+    Yevents.initViewMouseDownEvent("totop", "toright", "tobottom", "toleft")
     Yrenderer.renderer.render(Yscene.scene, Ycamera.camera)
   }
 
@@ -37,11 +56,38 @@ class MB extends Component {
     this.draw(width, height)
   }
 
+  switchHand() {
+    const MB = document.getElementById("MB")
+    this.state.handChoose = !this.state.handChoose
+    if (this.state.handChoose) {
+      MB.style.cursor = "pointer"
+      Yevents.initCanvasDragEvent()
+    } else {
+      MB.style.cursor = "default"
+      Yevents.dispatchCanvasDragEvent()
+    }
+    this.setState({
+      handChoose: this.state.handChoose
+    })
+  }
+
+  switchView(view) {
+    this.state.viewChoose = view
+    this.setState({
+      viewChoose: this.state.viewChoose
+    })
+  }
+
   render() {
     return (
       <div id="MB">
         {/* 控件区域 */}
         <div id="mainTop">
+          <Tooltip placement="bottom" title="拖拽画布">
+            <div className="btnItem" onClick={() => this.switchHand()}>
+              <img src={this.state.handChoose ? iconHandChoose : iconHand} alt=""/>
+            </div>
+          </Tooltip>
           <Tooltip placement="bottom" title="放大场景">
             <div className="btnItem" onClick={() => Ycamera.zoomAdd()}>
               <img src={iconAdd} alt=""/>
@@ -67,6 +113,38 @@ class MB extends Component {
 
         {/* 图层区域 */}
         <div id="mainRight"></div>
+
+        {/* 视角控件 */}
+        <div id="viewControl">
+          <div
+            id="totop"
+            onMouseDown={() => this.switchView('totop')}
+            onMouseUp={() => this.switchView("")}
+          >
+            <img src={this.state.viewChoose === 'totop' ? iconTopChoose : iconTop} alt=""/>
+          </div>
+          <div
+            id="toright"
+            onMouseDown={() => this.switchView('toright')}
+            onMouseUp={() => this.switchView("")}
+          >
+            <img src={this.state.viewChoose === 'toright' ? iconRightChoose : iconRight} alt=""/>
+          </div>
+          <div
+            id="tobottom"
+            onMouseDown={() => this.switchView('tobottom')}
+            onMouseUp={() => this.switchView("")}
+          >
+            <img src={this.state.viewChoose === 'tobottom' ? iconBottomChoose : iconBottom} alt=""/>
+          </div>
+          <div
+            id="toleft"
+            onMouseDown={() => this.switchView('toleft')}
+            onMouseUp={() => this.switchView("")}
+          >
+            <img src={this.state.viewChoose === 'toleft' ? iconLeftChoose : iconLeft} alt=""/>
+          </div>
+        </div>
       </div>
     );
   }
